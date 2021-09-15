62bb301a4009c96dc3203e8b0811fcbb55bbcf3c
Manel Baradad <mbaradad@mit.edu>
2021-09-15 17:35:59 -0400

    Added more ansible scripts

---
+++ ansible_utils/ansible_run_distributed_python_script/portilla_simoncelli.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+export HOME=/data/vision/torralba/movies_sfm/home
+cd $HOME
+
+#add my_libs
+export PATH="$HOME/programs/bin:$PATH"
+# added by Anaconda2 installer
+if [[ $PATH = *$HOME/anaconda* ]]
+then
+    echo "Anaconda path already present, wont be added again!"
+else
+    export PATH="$HOME/anaconda3/bin:$PATH"
+fi
+
+source activate default_env37
+source $HOME/.bash_commands/all
+cd $HOME/no_training_cnn/contrastive_image_models;
+export PYTHONPATH=..
+python image_generation/portilla_simoncelli/texture_synthesis.py
