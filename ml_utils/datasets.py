c1569cc1867cdcbdfbd8ac974fae5b066113fea3
Manel Baradad <manelbaradad@gmail.com>
2022-10-24 10:00:07 -0400

    added fft hypernet

--- ml_utils/datasets.py
+++ ml_utils/datasets.py
@@ -10,6 +10,28 @@ def default_loader(path):
   return Image.open(path).convert('RGB')
 
 
+class CombinedDataset(data.Dataset):
+  def __init__(self, datasets):
+    assert type(datasets) is list
+    self.datasets = datasets
+
+    self.i_to_dataset_and_sample = dict()
+
+    total_i = 0
+    for cur_dataset in datasets:
+      for k in range(len(cur_dataset)):
+        self.i_to_dataset_and_sample[total_i] = (k, cur_dataset)
+        total_i += 1
+
+    self.total_samples = total_i
+
+  def __len__(self):
+    return self.total_samples
+
+  def __getitem__(self, item):
+    i, dataset = self.i_to_dataset_and_sample[item]
+    return dataset[i]
+
 class ImageFilelist(data.Dataset):
   def __init__(self, imlist, transform=ToTensor(), image_loader=default_loader, return_filename=False):
     self.transform = transform
