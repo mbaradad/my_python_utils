cffdacc6eb8f4ba7be041cff3457ac14f74db8ea
Manel Baradad <manelbaradad@gmail.com>
2024-01-29 22:13:28 +0000

    asdf

--- plot_utils/plot_with_plotly.py
+++ plot_utils/plot_with_plotly.py
@@ -15,3 +15,23 @@ def write_and_view_figure(fig, title, output_dir, height=600, width=900, show_on
     imshow(figure, title=title)
   return output_file, figure
 
+
+if __name__ == '__main__':
+  # example on how to use plotly
+    import plotly.express as px
+
+    fig_seen = px.bar(
+        x=list(method_performance_seen.keys()), 
+        y=list(method_performance_seen.values()), 
+        labels={'x': 'Method', 'y': 'Performance'},
+        title='Seen Performance'
+    )
+
+    # Plot for Unseen Performance
+    fig_unseen = px.bar(
+        x=list(method_performance_unseen.keys()), 
+        y=list(method_performance_unseen.values()), 
+        labels={'x': 'Method', 'y': 'Performance'},
+        title='Unseen Performance'
+    )
+    fig_unseen.show()
