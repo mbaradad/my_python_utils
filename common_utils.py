bde565ce9e9a61b81d083c3dba9a14a4e95d4066
Manel Baradad <manelbaradad@gmail.com>
2024-05-29 11:47:54 -0400

    Changed use_pathos to map

--- common_utils.py
+++ common_utils.py
@@ -2995,7 +2995,7 @@ def process_in_parallel_or_not(function, elements, parallel, use_pathos=False, n
   if parallel:
     if use_pathos:
       pool = Pool()
-      return pool.apply(function, elements)
+      return pool.map(function, elements)
     if num_cpus > 0:
       return p_map(function, elements, num_cpus=num_cpus)
     else:
